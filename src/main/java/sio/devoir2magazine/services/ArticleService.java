cb21d8052f29579e8d6fdad4830057135f324ca4
Jerem Bttn <jeremy.bitton@ortmontreuil.fr>
2025-11-18 22:37:48 +0100

    init projet

---
+++ src/main/java/sio/devoir2magazine/services/ArticleService.java
@@ -0,0 +1,22 @@
+package sio.devoir2magazine.services;
+
+import org.springframework.stereotype.Service;
+import sio.devoir2magazine.dto.ArticlePigiste;
+import sio.devoir2magazine.entities.Article;
+import sio.devoir2magazine.repositories.ArticleRepository;
+
+import java.util.List;
+
+@Service
+public class ArticleService
+{
+    private ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public List<ArticlePigiste> getAllArticlePigistes(int numPig) {
+        return articleRepository.findArticlesByNumPigs(numPig);
+    }
+}
